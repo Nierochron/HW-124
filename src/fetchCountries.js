66423e3e1aa66920b9d217be302931be8d585ad6
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-12-10 18:51:53 +0100

    ...

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://restcountries.com/v3.1/name/"
+
+const fetchCountries = (searchQuery) => {
+
+}
+
+export default fetchCountries;
+
+// https://restcountries.com/v3.1/name/{name}
